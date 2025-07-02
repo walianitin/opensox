086e08ae8b527f863d3c39d08abda1c61f5dfdaa
walianitin <walianitin828@gmail.com>
2025-07-03 00:50:29 +0530

    added

--- app/utils/httpsclient.tsx
+++ app/utils/httpsclient.tsx
@@ -1,12 +1,19 @@
 
 import axios from "axios";
 import { Organisations, Org } from "./types";
+import { headers } from "next/headers";
 const Base_Url="https://api.github.com";
-
+// Use process.env.GITHUB_API_TOKEN directly in your code
 
 
 export async function  getOrganisation():Promise<Organisations[]>{
-    const response =await axios.get(`${Base_Url}/organizations`)
+    const response =await axios.get(`${Base_Url}/organizations`,
+        {
+            headers: {
+                Authorization: process.env.GITHUB_API_TOKEN
+            }
+        }
+    )
     const data= response.data.sort((a:Organisations,b:Organisations)=>{
             return 0
     });
@@ -15,17 +22,26 @@ export async function  getOrganisation():Promise<Organisations[]>{
 }
 
 export async function getinsideData(url:string) {
-    const response= await axios.get(`${url}`)
+    const response= await axios.get(`${url}`,{
+        headers:{
+            Authorization: process.env.GITHUB_API_TOKEN
+        }
+    })
     const data:Org=response.data;
     return data;
     
 }
 
 export async function  getIssues(url:string) {
-    const repos_url= await axios.get(`${url}`);
+    const repos_url= await axios.get(`${url}`, {
+            headers: {
+                Authorization: process.env.GITHUB_API_TOKEN
+            }
+        }
+    );
 
     const data=repos_url.data;
-    return 
+    return  data;
     
 }
 
